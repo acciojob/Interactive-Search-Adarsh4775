d5e26c6a22cdef02d7f7eb7a585383dc288bc4c7
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-26 19:03:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function intSearch(){
-	const submit = document.querySelector(".btn");
+	const submit = document.querySelector(".input");
 
 	submit.addEventListener("click",function() {
          document.querySelector(".search").classList.add("active") ;
