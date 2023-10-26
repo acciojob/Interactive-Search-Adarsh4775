dbc3c1984134f19caeec2c5a14a8a4f3bfab8747
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-26 17:46:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function intSearch(){
+	const search = document.getElemntById("input");
+	const submit = document.getElemntById("btn");
+
+	btn.addEventListener("click",function() {
+            
+	});
+}
